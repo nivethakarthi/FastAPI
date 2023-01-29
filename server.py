53e0581ff1b805ce275119cf0af415159d23f919
Nivetha Karthikeyan <68457848+nivethakarthi@users.noreply.github.com>
2023-01-29 17:34:41 +0100

    Update server.py

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ async def shorten_url(url: str = Form(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(__name__ + ":app", host="127.0.0.1", port=8888, log_level="info", reload=True)
+    uvicorn.run(__name__ + ":app", host="0.0.0.0", port=8888, log_level="info", reload=True)
 
 
 
